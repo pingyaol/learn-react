fd3f63149286470991e05967c9dd538aba0981c0
Pingyao Liu <pingyaoliu@daisyzhangs-MacBook-Pro.local>
2024-06-18 11:50:54 -0700

    pt 2

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,17 +4,25 @@ import Food from "./Food";
 import Card from "./Card";
 import Button from "./Button";
 import Student from "./Student";
+import UserGreeting from "./UserGreeting";
 
 function App() {
-
   return(
     <>
-      <Card />
-      <Button />
-      <Student name="Pingyao" age={30} isStudent={true}/>
+      <UserGreeting isLoggedIn={true} username="BroCode" /> 
     </>
-    
   ); 
 }
 
 export default App
+
+/*
+<Card />
+      <Button />
+      <Student name="Abby" age={30} isStudent={true}/>
+      <Student name="Bessie" age={16} isStudent={true}/>
+      <Student name="Callie" age={16} isStudent={true}/>
+      <Student name="Debbie" age={17} isStudent={true}/>
+      <Student  />
+
+*/ 
